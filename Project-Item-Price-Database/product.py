4a4f3a8ad652193e073754de76e6c586a783a2de
AlexanderJarvheden <alexander.jarvheden@gmail.com>
2024-01-02 13:57:14 +0100

    Remove moved files

--- Project-Item-Price-Database/product.py
+++
@@ -1,6 +0,0 @@
-class Product:
-    """Creates an item and its information"""
-    def __init__(self, code, name, price):
-        self.code = code
-        self.name = name
-        self.price = price
